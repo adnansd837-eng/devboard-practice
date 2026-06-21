c1a04941302e264e6ef2bb16e3951b9807df0eab
adnan-syed <adnansd837@gmail.com>
2026-06-21 18:05:29 +0000

    added files

---
+++ nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update package list
+sudo apt update -y
+
+# Install Nginx
+sudo apt install nginx -y
+
+# Start Nginx service
+sudo systemctl start nginx
+
+# Enable Nginx to start automatically on boot
+sudo systemctl enable nginx
+
+# Check Nginx status
+sudo systemctl status nginx
